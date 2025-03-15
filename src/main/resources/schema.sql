e8fd8bb1bd3bc790e2e60b1e18b0b37ce7640058
h.j_west <guswl9814@gmail.com>
2025-03-15 11:11:35 +0900

    초기 테이블 쿼리 작성(short_url, url_access_log)

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,20 @@ create table Member
     name varchar(255) not null,
     primary key (id)
 );
+
+DROP TABLE IF EXISTS SHORT_URL;
+
+CREATE TABLE SHORT_URL (
+                           ID INTEGER AUTO_INCREMENT PRIMARY KEY,
+                           ORI_URL VARCHAR(2048) NOT NULL,
+                           SHORT_URL VARCHAR(8) NOT NULL UNIQUE,
+                           CREATED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+DROP TABLE IF EXISTS URL_ACCESS_LOG;
+
+CREATE TABLE URL_ACCESS_LOG (
+                                ID INTEGER AUTO_INCREMENT PRIMARY KEY,
+                                SHORT_URL VARCHAR(8) NOT NULL,
+                                ACCESSED_AT TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
