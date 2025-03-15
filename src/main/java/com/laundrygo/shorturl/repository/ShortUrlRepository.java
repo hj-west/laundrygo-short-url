9217b03c4c30d5c9a87a1beca100d0521303c1d3
h.j_west <guswl9814@gmail.com>
2025-03-15 13:38:14 +0900

    조회용 메서드 추가(ShortUrlRepository.findByOriUrl, ShortUrlRepository.findByShortUrl)

--- src/main/java/com/laundrygo/shorturl/repository/ShortUrlRepository.java
+++ src/main/java/com/laundrygo/shorturl/repository/ShortUrlRepository.java
@@ -4,7 +4,12 @@ import com.laundrygo.shorturl.domain.ShortUrl;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
+
 @Mapper
 public interface ShortUrlRepository extends JpaRepository<ShortUrl, Long> {
+    Optional<ShortUrl> findByOriUrl(String oriUrl);
+    Optional<ShortUrl> findByShortUrl(String shortUrl);
 }
 
