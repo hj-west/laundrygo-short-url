5e7e18133eeb109952c51a1518a3459b16ba72f8
h.j_west <guswl9814@gmail.com>
2025-03-16 13:43:34 +0900

    OriUrl 조회 API 통합 테스트 구현

--- src/test/java/com/laundrygo/shorturl/ShortUrlIntegrationTest.java
+++ src/test/java/com/laundrygo/shorturl/ShortUrlIntegrationTest.java
@@ -3,9 +3,8 @@ package com.laundrygo.shorturl;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.laundrygo.shorturl.dto.CreateShortUrlRequestDto;
-import com.laundrygo.shorturl.dto.CreateShortUrlResponseDto;
 import com.laundrygo.shorturl.repository.ShortUrlRepository;
-import org.junit.jupiter.api.BeforeEach;
+import com.laundrygo.shorturl.service.ShortUrlService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,6 +14,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -33,7 +34,7 @@ class ShortUrlIntegrationTest {
     private ObjectMapper objectMapper;
 
     @Test
-    @DisplayName("Short Url 생성 API 통합 테스트")
+    @DisplayName("Short Url 생성 & 조회 API 통합 테스트")
     void createShortUrlIntegrationTest() throws Exception {
         // Given
         String oriUrl = "https://www.example.com";
@@ -58,6 +59,17 @@ class ShortUrlIntegrationTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.oriUrl").value(oriUrl))
                 .andExpect(jsonPath("$.shortUrl").value(shortUrl));
+
+        // When & Then - 3. OriUrl 조회
+        mockMvc.perform(get("/urls/" + shortUrl)
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().string(oriUrl));
+
+        // When & Then - 4. 존재하지 않는 ShortUrl 요청
+        mockMvc.perform(get("/urls/invalidShortUrl")
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(content().string("Not Found Ori Url. request short url - invalidShortUrl"));
 
         assert shortUrlRepository.findByOriUrl(oriUrl).isPresent();
     }
