abde74be8d9b472da4c01ebee942b6aa6c99a495
h.j_west <guswl9814@gmail.com>
2025-03-16 13:10:53 +0900

    시간별 조회용 메서드 추가

--- src/main/java/com/laundrygo/shorturl/repository/UrlAccessLogRepository.java
+++ src/main/java/com/laundrygo/shorturl/repository/UrlAccessLogRepository.java
@@ -4,6 +4,10 @@ import com.laundrygo.shorturl.domain.UrlAccessLog;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Mapper
 public interface UrlAccessLogRepository extends JpaRepository<UrlAccessLog, Long> {
+    List<UrlAccessLog> findByShortUrlAndAccessAtAfter(String shortUrl, LocalDateTime time);
 }
